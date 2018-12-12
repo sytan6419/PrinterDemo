5f575e9cfbfb0857dea2071a84fa6bd2dcbfe3ec
sytan6419 <sytan6419@gmail.com>
2018-12-13 00:45:33 +0800

    Initial CheckIn

---
+++ PrinterDemo/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PrinterDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btn_print = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(199, 112);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(266, 120);
+            this.btn_print.TabIndex = 0;
+            this.btn_print.Text = "Print";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 367);
+            this.Controls.Add(this.btn_print);
+            this.Name = "Form1";
+            this.Text = "Printer Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btn_print;
+    }
+}
+
---
+++ PrinterDemo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace PrinterDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // init serial com port at here
+            // Todo: set the arduino COM port at here. Check at device manager
+            SerialPort mySerialPort = new SerialPort("COM1");
+
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            mySerialPort.Open();
+        }
+
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            //TSCLIB_DLL.about();                                                                 //Show the DLL version
+            TSCLIB_DLL.openport("TSC TDP-225");                                           //Open specified printer driver
+            TSCLIB_DLL.setup("100", "63.5", "4", "8", "0", "0", "0");                           //Setup the media size and sensor type info
+            TSCLIB_DLL.clearbuffer();                                                           //Clear image buffer
+            TSCLIB_DLL.barcode("100", "100", "128", "100", "1", "0", "2", "2", "Barcode Test"); //Drawing barcode
+            TSCLIB_DLL.printerfont("100", "250", "3", "0", "1", "1", "Print Font Test");        //Drawing printer font
+            TSCLIB_DLL.windowsfont(100, 300, 24, 0, 0, 0, "ARIAL", "Windows Arial Font Test");  //Draw windows font
+            TSCLIB_DLL.downloadpcx("UL.PCX", "UL.PCX");                                         //Download PCX file into printer
+            TSCLIB_DLL.sendcommand("PUTPCX 100,400,\"UL.PCX\"");                                //Drawing PCX graphic
+            TSCLIB_DLL.printlabel("1", "1");                                                    //Print labels
+            TSCLIB_DLL.closeport();                                                             //Close specified printer driver 
+        }
+
+        private static void DataReceivedHandler(
+                            object sender,
+                            SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+
+            // If received "Detected!" from arduino Port
+            if (indata == "Detected!")
+            {
+                // Setup printer to print something
+                TSCLIB_DLL.openport("TSC TDP-225");                                           //Open specified printer driver
+                TSCLIB_DLL.setup("100", "63.5", "4", "8", "0", "0", "0");                           //Setup the media size and sensor type info
+                TSCLIB_DLL.clearbuffer();                                                           //Clear image buffer
+                TSCLIB_DLL.barcode("100", "100", "128", "100", "1", "0", "2", "2", "Barcode Test"); //Drawing barcode
+                TSCLIB_DLL.printerfont("100", "250", "3", "0", "1", "1", "Print Font Test");        //Drawing printer font
+                TSCLIB_DLL.windowsfont(100, 300, 24, 0, 0, 0, "ARIAL", "Windows Arial Font Test");  //Draw windows font
+                TSCLIB_DLL.downloadpcx("UL.PCX", "UL.PCX");                                         //Download PCX file into printer
+                TSCLIB_DLL.sendcommand("PUTPCX 100,400,\"UL.PCX\"");                                //Drawing PCX graphic
+                TSCLIB_DLL.printlabel("1", "1");                                                    //Print labels
+                TSCLIB_DLL.closeport();
+            }
+        }
+    }
+}
