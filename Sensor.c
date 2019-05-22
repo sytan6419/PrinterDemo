0d7a209e5a3a55586127519f3ca8a1c97cb7007b
sytan6419 <sytan6419@gmail.com>
2019-05-22 12:52:58 +0800

    Update Sensor.c
    Changed port

--- Sensor.c
+++ Sensor.c
@@ -6,7 +6,7 @@
  */
 
 // this constant won't change:
-const int  buttonPin = 8;    // the pin that the pushbutton is attached to
+const int  buttonPin = 5;    //8    // the pin that the pushbutton is attached to
 const int ledPin = 13;       // the pin that the LED is attached to
 
 // Variables will change:
